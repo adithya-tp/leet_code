385750efebbedb65a98f54b42d92c46faa7b679c
adithya-tp <adithyatp@yahoo.com>
2019-10-10 23:31:43 +0400

    Brute force solutions(will not get accepted) - both received TLE

--- 0581_shortest_unsorted_continuous_subarray/solution01_brute_force_alternative.cpp
+++ 0581_shortest_unsorted_continuous_subarray/solution01_brute_force_alternative.cpp
@@ -1,33 +1,29 @@
-public class Solution {
-    public int findUnsortedSubarray(int[] nums) {
-        int res = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j <= nums.length; j++) {
-                int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, prev = Integer.MIN_VALUE;
-                for (int k = i; k < j; k++) {
-                    min = Math.min(min, nums[k]);
-                    max = Math.max(max, nums[k]);
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& v) {
+        int min = v.size();
+        int v_size = v.size();
+        int max_unsorted1, min_subarray, max_subarray, min_unsorted2;
+        if(is_sorted(v.begin(), v.end()))
+            return 0;
+        for(int i = 0; i < v.size(); i++){
+            for(int j = i + 1; j < v.size() + 1; j++){
+                 vector<int> temp = v;
+                vector<int> sub_array(temp.begin() + i, temp.begin() + j);
+                int unsorted1_size = i, unsorted2_size = v_size - j;
+                sort(temp.begin(), temp.begin() + i);
+                sort(temp.begin() + j, temp.end());
+                sort(sub_array.begin(), sub_array.end());
+                max_unsorted1 = (unsorted1_size == 0) ? 0 : temp[i - 1];
+                min_subarray = sub_array[0];
+                max_subarray = sub_array[sub_array.size() - 1];
+                min_unsorted2 = (unsorted2_size == 0) ? max_subarray : temp[j];
+                if((max_unsorted1 <= min_subarray) && (max_subarray <= min_unsorted2) && is_sorted(v.begin(), v.begin() + i) && is_sorted(v.begin() + j, v.end())){
+                    if(sub_array.size() < min)
+                        min = sub_array.size();
                 }
-                if ((i > 0 && nums[i - 1] > min) || (j < nums.length && nums[j] < max))
-                    continue;
-                int k = 0;
-                while (k < i && prev <= nums[k]) {
-                    prev = nums[k];
-                    k++;
-                }
-                if (k != i)
-                    continue;
-                k = j;
-                while (k < nums.length && prev <= nums[k]) {
-                    prev = nums[k];
-                    k++;
-                }
-                if (k == nums.length) {
-                    res = Math.min(res, j - i);
-
-                }
-            }
+            }        
         }
-        return res;
+        return min;
     }
-}
+};
