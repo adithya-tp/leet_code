72c071c95dc8c6395c9d4bed2a4c53d973b585ed
adithya-tp <adithyatp@yahoo.com>
2019-10-11 14:04:57 +0400

    LeetCode Single Number

--- 0136_single_number/solution.cpp
+++ 0136_single_number/solution.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int res = nums[0];
+	//solution using xor
         for(int i = 1; i < nums.size(); i++){
             res = res ^ nums[i];
         }
