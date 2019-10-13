c63022c1b0e0202eb433265a1b9afcc72f089264
adithya-tp <adithyatp@yahoo.com>
2019-10-13 17:49:04 +0400

    Better brute force

--- 0581_shortest_unsorted_continuous_subarray/solution02_brute_force.cpp
+++ 0581_shortest_unsorted_continuous_subarray/solution02_brute_force.cpp
@@ -1,17 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    vector<int> v = {2,1};
-    int r = 0, l = v.size();
-    for(int i = 0; i < v.size() - 1; i++){
-	for(int j = i + 1; j < v.size(); j++){
-	    if(v[j] < v[i]){
-		r = max(j, r);
-		l = min(i, l);
-	    }
-	}
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& v) {
+        int r = 0, l = v.size();
+        for(int i = 0; i < v.size() - 1; i++){
+            for(int j = i + 1; j < v.size(); j++){
+                if(v[j] < v[i]){
+                    r = max(j, r);
+                    l = min(i, l);
+                }
+            }
+        }
+        return (r - l) < 0 ? 0 : r - l + 1;
     }
-    int res = (r - l) < 0 ? 0 : r - l + 1;
-    cout << res << endl;
-    return 0;
-}
+};
