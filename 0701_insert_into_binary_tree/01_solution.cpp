4017542242ae0f3c89ceaf82419705918c8f8525
adithya-tp <adithyatp@yahoo.com>
2020-05-01 08:50:35 +0400

    LeetCode Insert Into Binary Tree

---
+++ 0701_insert_into_binary_tree/01_solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(val > root->val) {
+            if(root->right == NULL) {
+                root->right = new TreeNode(val);
+                return root;
+            }
+            else {
+                insertIntoBST(root->right, val);
+            }   
+        }
+        else {
+            if(root->left == NULL) {
+                root->left = new TreeNode(val);
+                return root;
+            }
+            else {
+                insertIntoBST(root->left, val);       
+            }
+        }
+        return root;
+    }
+};
