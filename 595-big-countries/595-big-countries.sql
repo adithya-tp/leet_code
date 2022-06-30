fbf4e90ead5d9bd03b5461857313f52358b704a7
Adithya T. Praveen <adithyatp@yahoo.com>
2022-06-30 12:54:26 -0400

    Time: 300 ms (60.03%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,4 +1,7 @@
-# blah blah
-select name, population, area 
-from World 
-where area >= 3000000 or population >= 25000000
+select 
+    name, population, area
+from 
+    World
+where 
+    area >= 3000000 OR population >= 25000000
+;
