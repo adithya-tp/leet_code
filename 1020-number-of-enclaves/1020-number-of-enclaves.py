aa876509a998f9cbbd82e84eb152a93e3a3c7cbc
Adithya T. Praveen <adithyatp@yahoo.com>
2022-07-01 10:55:39 -0400

    Time: 1345 ms (11.76%), Space: 15.5 MB (65.32%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,22 +1,15 @@
 class Solution:
-    def __init__(self):
-        self.count = 0
-    
     def dfs(self, g, r, c):
         rows, cols = len(g), len(g[0])
-        if (r < 0) or (c < 0) or (r >= rows) or (c >= cols):
-            return True
-        elif g[r][c] != 1:
-            return False
-        
-        self.count += 1
-        g[r][c] = -1
+        if (r < 0) or (c < 0) or (r >= rows) or (c >= cols) or (g[r][c] != 1):
+            return 0
+        g[r][c] = 0
         dx = [-1, 0, 1, 0]
         dy = [0, 1, 0, -1]
-        curr = False
+        ans = 1
         for k in range(4):
-            curr = self.dfs(g, r + dx[k], c + dy[k]) or curr
-        return curr
+            ans += self.dfs(g, r + dx[k], c + dy[k])
+        return ans
     
     
     def numEnclaves(self, grid: List[List[int]]) -> int:
@@ -24,10 +17,13 @@ class Solution:
         ans = 0
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 1:
-                    oob = self.dfs(grid, i, j)
-                    if not oob:
-                        ans += self.count
-                    self.count = 0
-                    
+                # Flood fill from the boundary land cells to 0
+                if (grid[i][j] == 1) and ((i == 0) or (j == 0) or (i == rows - 1) or (j == cols - 1)):
+                    self.dfs(grid, i, j)
+        
+        for i in range(rows):
+            for j in range(cols):
+                # Add up the number of remaining island sizes
+                if grid[i][j]:
+                    ans += self.dfs(grid, i, j)
         return ans
