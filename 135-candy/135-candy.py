65fd26d7cd4116ff32a2817f4e3177bf932d10fe
Adithya T. Praveen <adithyatp@yahoo.com>
2022-07-04 15:49:00 -0400

    Time: 254 ms (46.45%), Space: 16.9 MB (18.08%) - LeetHub

---
+++ 135-candy/135-candy.py
@@ -0,0 +1,13 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        ans_list = [1 for _ in range(n)]
+        for idx in range(0, n - 1):
+            if (ratings[idx + 1] > ratings[idx]):
+                ans_list[idx + 1] = ans_list[idx] + 1
+        
+        for idx in range(n - 1, 0, -1):
+            if (ratings[idx - 1] > ratings[idx]) and (ans_list[idx - 1] <= ans_list[idx]):
+                ans_list[idx - 1] = ans_list[idx] + 1
+                
+        return sum(ans_list)
