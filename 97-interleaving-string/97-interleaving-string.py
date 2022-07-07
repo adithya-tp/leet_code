f9049aa0022a785a990fde07b680369ac79dc923
Adithya T. Praveen <adithyatp@yahoo.com>
2022-07-06 23:45:11 -0400

    Time: 61 ms (59.28%), Space: 15.4 MB (14.25%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -28,19 +28,6 @@ class Solution:
         if (len(s1) + len(s2) != len(s3)):
             return False
         
-        if s1 + s2 == s3:
-            return True
-        
-        c = [0 for _ in range(26)]
-        for ch in ''.join([s1, s2]):
-            c[ord(ch) - ord('a')] += 1
-        
-        for ch in s3:
-            c[ord(ch) - ord('a')] -= 1
-            
-        if any(c):
-            return False
-        
         self.s1 = s1
         self.s2 = s2
         self.s3 = s3
