f76a0325f1cb6824724ac3f8b6564efc336eb25d
Adithya T. Praveen <adithyatp@yahoo.com>
2022-07-12 18:56:00 -0400

    Time: 959 ms (51.29%), Space: 29 MB (79.01%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -10,22 +10,19 @@ class Solution:
     def _partition(self, idx, rem_string):
         if len(rem_string) == 1:
             self.curr_list.append(rem_string)
-            self.ans.append(self.curr_list.copy()[:-1])
+            self.ans.append(self.curr_list.copy()[1:-1])
             self.curr_list.pop()
             return
         
         s1 = rem_string[:idx + 1]
         s2 = rem_string[idx + 1: ]
-        # print(s1, "->", s2)
-        if (idx != -1) and not (self._is_pal(s1)):
+        if not (self._is_pal(s1)):
             return
         
-        if len(s1):
-            self.curr_list.append(s1)
+        self.curr_list.append(s1)
         for next_idx in range(len(s2)):
             self._partition(next_idx, s2)
-        if len(s1):
-            self.curr_list.pop()
+        self.curr_list.pop()
         
     
     def partition(self, s: str) -> List[List[str]]:
