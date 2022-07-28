66b8c508a708fb66998ea6f9d9506462fdc7d9c5
Adithya T. Praveen <adithyatp@yahoo.com>
2022-07-28 18:01:29 -0400

    Time: 265 ms (82.81%), Space: 16.4 MB (57.44%) - LeetHub

---
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isTargetWordInSource(self, src, trg):
+        trg_ptr = 0
+        trg_len = len(trg)
+        for char in src:
+            if char == trg[trg_ptr]:
+                trg_ptr += 1
+                if trg_ptr == trg_len:
+                    return True
+        return False
+            
+    
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        max_len = 0
+        max_len_word = ""
+        for word in dictionary:
+            if self.isTargetWordInSource(s, word):
+                curr_word_len = len(word)
+                if curr_word_len > max_len:
+                    max_len = curr_word_len
+                    max_len_word = word
+                elif curr_word_len == max_len:
+                    if word < max_len_word:
+                        max_len_word = word
+        return max_len_word
