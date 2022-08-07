039c768974c9f136d2809ea80241c73586b0b9b4
Adithya T. Praveen <adithyatp@yahoo.com>
2022-08-07 11:20:45 -0400

    Time: 1868 ms (9.20%), Space: 155.6 MB (22.65%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -0,0 +1,37 @@
+class Solution:
+    def __init__(self):
+        self.n = None
+        self.ans = 0
+        self.mod = int(1e9 + 7)
+        self.curr_string = []
+        self.map = {
+            ' ': ['a','e','i','o','u'],
+            'a': ['e'],
+            'e': ['a', 'i'],
+            'i': ['a', 'e', 'o', 'u'],
+            'o': ['i', 'u'],
+            'u': ['a']
+        }
+        self.dp = None
+
+    def brute_force(self, idx, last_char): #, curr_perm_size):
+        if idx == self.n:
+            return 1
+            
+        if self.dp[last_char][idx] != -1:
+            return self.dp[last_char][idx]
+        
+        ans = 0
+        next_list = self.map[last_char]
+        for next_char in next_list:
+            self.curr_string.append(next_char)
+            ans += self.brute_force(idx + 1, next_char)
+            self.curr_string.pop()
+        self.dp[last_char][idx] = ans
+        return ans
+    
+    def countVowelPermutation(self, n: int) -> int:        
+        self.n = n
+        self.dp = {k: [-1 for _ in range(n)] for k in ' aeiou'}
+        self.brute_force(0, ' ')
+        return self.dp[' '][0] % self.mod
